2de830b236fe49cfbe4ea8ff3a5b980afefdcbff
oneTime999 <onetime.zkk@gmail.com>
2025-09-19 18:04:06 -0300

    Update Services.lua

--- Services.lua
+++ Services.lua
@@ -1,17 +1,13 @@
--- Services.lua - Módulo para gerenciar serviços do Roblox
--- Centraliza todos os serviços em um local
+-- Services.lua - Centralização dos serviços do Roblox
 
 local Services = {}
 
--- Serviços do Roblox
 Services.Players = game:GetService("Players")
 Services.ReplicatedStorage = game:GetService("ReplicatedStorage")
 Services.Workspace = game:GetService("Workspace")
 Services.UserInputService = game:GetService("UserInputService")
 Services.TweenService = game:GetService("TweenService")
 
--- Player local e GUI
 Services.LocalPlayer = Services.Players.LocalPlayer
-Services.playerGui = Services.LocalPlayer:WaitForChild("PlayerGui")
 
 return Services
